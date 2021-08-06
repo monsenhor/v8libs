7feaf4403d6c6855e2c7209863b36bf507b07abf
Monsenhor <filipo@kobkob.org>
2021-08-06 13:07:25 -0400

    Typos in get_request example

--- get_request/server.js
+++ get_request/server.js
@@ -1,15 +1,17 @@
-const http = require('http')
-const url = require('url')
+/* jshint esversion: 6 */
 
-const PORT = 3000
+const http = require('http');
+const url = require('url');
 
-console.log("Server listening at http://127.0.0.1:"+PORT)
+const PORT = 3000;
+
+console.log("Server listening at http://127.0.0.1:"+PORT);
 
 http.createServer(function (req, res) {
   const queryObject = url.parse(req.url,true).query;
-  var html_code = '<h3>Hello World!</h3>'
+  var html_code = '<h3>Hello World!</h3>';
   if (queryObject.name){
-     html_code = '<h3>Helllo, '+queryObject.name+'!</h3>'    
+     html_code = '<h3>Helllo, '+queryObject.name+'!</h3>';
   }
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(html_code);
